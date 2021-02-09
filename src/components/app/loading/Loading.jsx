a7853fb5860c48b0372624709cf31fa20d76e351
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-09 14:36:17 -0800

    adds Loading

---
+++ src/components/app/loading/Loading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Loading = () => (
+    <img src="http://static.demilked.com/wp-content/uploads/2016/06/gif-animations-replace-loading-screen-2.gif" alt="loading" />
+)
+
+export default Loading;
