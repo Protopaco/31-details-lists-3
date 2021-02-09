9542fc94f126479d0154ea1a54f7bab31f2252b0
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-09 15:00:09 -0800

    updates App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import CountryByCode from '../../containers/CountryByCode';
+import CountriesPage from '../../containers/CountriesPage';
+
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={CountriesPage} />
+        <Route path="/country/:id" component={CountryByCode} />
+      </Switch>
+    </Router>
+  );
 }
